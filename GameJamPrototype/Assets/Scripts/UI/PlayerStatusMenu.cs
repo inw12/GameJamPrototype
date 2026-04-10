c7fb65894f8af8dd3e1ae93f5c43eaac14894d73
iann <ianathanwong@gmail.com>
2026-04-09 18:31:39 -0700

    Status Menu Polish
    Adjusted stick figure proportions

--- GameJamPrototype/Assets/Scripts/UI/PlayerStatusMenu.cs
+++ GameJamPrototype/Assets/Scripts/UI/PlayerStatusMenu.cs
@@ -5,19 +5,30 @@ public class PlayerStatusMenu : MonoBehaviour
     // Inputs
     private bool MenuActive => PlayerControls.Instance.StatusMenu;
 
+    [Header("Menu Container")]
     [SerializeField] private RectTransform container;
     [SerializeField] private float activeSize;
     [SerializeField] private Vector3 activeOffset;
     [SerializeField] private float menuSpeed;
     private Vector3 _defaultSize;
     private Vector3 _defaultPosition;
-    [Space]
+
+    [Header("Body Part Buttons")]
     [SerializeField] private Button head;
     [SerializeField] private Button body;
     [SerializeField] private Button armL;
     [SerializeField] private Button armR;
     [SerializeField] private Button legL;
     [SerializeField] private Button legR;
+
+    [Header("Regeneration Meter")]
+    [SerializeField] private Image fillMeter;
+    [SerializeField] private Image regenerationMeter;
+    [SerializeField] private float fillSpeed;
+
+    public float currentRegen;
+    public float maxRegen;
+    private float _regenTimer;
 
     void Start()
     {
