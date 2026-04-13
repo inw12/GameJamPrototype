b8263eb10046b22fd0d687e07265777a06b2f96e
iann <ianathanwong@gmail.com>
2026-04-13 11:56:14 -0700

    WeaponPickup Script

---
+++ GameJamPrototype/Assets/Scripts/Weapons/WeaponPickup.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+[RequireComponent(typeof(CircleCollider2D))]
+public class WeaponPickup : MonoBehaviour
+{
+    public string PromptText { get; private set; }
+
+    [SerializeField] private GameObject weaponCounterpart;
+    [SerializeField] private float pickupRadius;
+
+    private CircleCollider2D _collider;
+
+    void Start()
+    {
+        PromptText = "(E)";
+
+        _collider = GetComponent<CircleCollider2D>();
+        _collider.radius = pickupRadius;
+    }
+
+    // returns GameObject to be collected 
+    public GameObject GetItem() => weaponCounterpart;
+
+    // turns on/off highlight around sprite to let player know
+    // that the object is/isn't pick up-able
+    public void ToggleHighlight()
+    {
+        
+    }
+}
